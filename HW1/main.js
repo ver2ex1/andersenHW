6810367f04c6368ba7be456ca9c9f638351b3314
Vadym Hlushko <ver2ex@MacBook-Air-Vadym.local>
2021-09-29 19:06:37 +0300

    HW1 done

---
+++ HW1/main.js
@@ -0,0 +1,30 @@
+function numToScale () {
+    try {
+    var num = +prompt('Введите число')
+    var scale = +prompt('Введите систему счисления')
+    console.log( typeof num.toString(scale))
+    }
+    catch (error) {
+        console.log('Некорректный ввод!')
+    }
+}
+
+function sumAndDividing () {
+    let firstValue = +prompt('Введите первое значение')
+      if (isNaN(firstValue)) {
+        console.log('Некорректный ввод!') 
+        return
+        }
+    let secondValue = +prompt('Введите второе значение')
+      if (isNaN(secondValue)) {
+        console.log('Некорректный ввод!')
+        return
+        } 
+      else {
+        console.log(firstValue + secondValue , firstValue / secondValue)  
+        }
+      }
+
+
+
+
